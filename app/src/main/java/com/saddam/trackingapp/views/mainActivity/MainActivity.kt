e9b4741f8e2f53c6606887fd59d4a29207dda1b5
SYSTEMS\saddam.hussaintaj <saddam.hussaintaj@systemlt.com>
2023-02-01 17:52:22 +0500

    binding set in activity

--- app/src/main/java/com/saddam/trackingapp/views/mainActivity/MainActivity.kt
+++ app/src/main/java/com/saddam/trackingapp/views/mainActivity/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.saddam.trackingapp.views.mainActivity
 
+import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
@@ -9,20 +11,23 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.saddam.trackingapp.R
+import com.saddam.trackingapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
 
-    lateinit var bottomNavigationView:BottomNavigationView
+
+    lateinit var  binding: ActivityMainBinding
 
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val layoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        bottomNavigationView = findViewById(R.id.bottom_nav)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
@@ -30,7 +35,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, AppBarConfiguration(setOf(R.id.id_setup,R.id.id_statistic,R.id.id_setting)))
 
 
-        bottomNavigationView.setupWithNavController(navController)
+        binding.bottomNav.setupWithNavController(navController)
 
 
 
