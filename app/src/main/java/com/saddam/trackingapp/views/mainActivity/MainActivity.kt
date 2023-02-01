1514d16cc82574270df979749f27c8ed6340576b
SYSTEMS\saddam.hussaintaj <saddam.hussaintaj@systemlt.com>
2023-02-01 17:35:09 +0500

    bottom navigation set for the navigation graph.

--- app/src/main/java/com/saddam/trackingapp/views/mainActivity/MainActivity.kt
+++ app/src/main/java/com/saddam/trackingapp/views/mainActivity/MainActivity.kt
@@ -1,21 +1,46 @@
 package com.saddam.trackingapp.views.mainActivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.saddam.trackingapp.R
-import com.saddam.trackingapp.db.RunDao
+import com.saddam.trackingapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-   private val viewModel:MainViewModel by viewModels()
+
+    lateinit var bottomNavigationView:BottomNavigationView
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController, AppBarConfiguration(setOf(R.id.id_setup,R.id.id_statistic,R.id.id_setting)))
+
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
+
     }
+
+
 }
